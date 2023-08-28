4e5dddc62e63d39c1da7954ecbcba7d9a36f1f25
Tarun <srfashions.tarun@gmail.com>
2023-08-28 20:38:40 +0530

    remove np_utils

--- keras_en_parser_and_analyzer/library/classifiers/lstm.py
+++ keras_en_parser_and_analyzer/library/classifiers/lstm.py
@@ -3,7 +3,7 @@ from keras.layers import Embedding, SpatialDropout1D, LSTM, Dense, Bidirectional
 from keras.models import model_from_json, Sequential
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
-from keras.utils import np_utils
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
 from keras_en_parser_and_analyzer.library.utility.tokenizer_utils import word_tokenize
@@ -227,7 +227,7 @@ class WordVecLstmSoftmax(object):
             ys.append(self.labels[label])
 
         X = pad_sequences(xs, maxlen=self.max_len)
-        Y = np_utils.to_categorical(ys, len(self.labels))
+        Y = to_categorical(ys, len(self.labels))
 
         x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state)
         print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
@@ -357,7 +357,7 @@ class WordVecBidirectionalLstmSoftmax(object):
             ys.append(self.labels[label])
 
         X = pad_sequences(xs, maxlen=self.max_len)
-        Y = np_utils.to_categorical(ys, len(self.labels))
+        Y = to_categorical(ys, len(self.labels))
 
         x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state)
         print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
